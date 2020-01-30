19579594aa1dad531a76648e5c5f4bf66f3e3d46
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-01-30 17:20:49 +0300

    Develop (#10)
    * Feature/01 first skeleton (#1)
    
    * item 001
    
    * the first skeleton
    
    * Feature/01 first skeleton (#3)
    
    * item 001
    
    * the first skeleton
    
    * the first test
    
    * red-green (#5)
    
    * red-green-refactor (#6)
    
    * after refactoring of the tests
    
    * with encoding error (#8)
    
    * finished (#9)

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"image/png"
 	"testing"
 )
 
-func TestGenerateQRCodeReturnsValue(t *testing.T) {
+func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 	buffer := new(bytes.Buffer)
 	GenerateQRCode(buffer, "555-2368")
 
 	if buffer.Len() == 0 {
 		t.Errorf("No QRCode generated")
 	}
-}
-
-func TestGenerateQRCodeGenratesPNG(t *testing.T) {
-	buffer := new(bytes.Buffer)
-	GenerateQRCode(buffer, "555-2368")
 	_, err := png.Decode(buffer)
 
 	if err != nil {
 		t.Errorf("Generated QRCode is not a PNG: %s", err)
+	}
+}
+
+type ErrorWriter struct{}
+
+func (e *ErrorWriter) Write(b []byte) (int, error) {
+	return 0, errors.New("Expected error")
+}
+
+func TestGenerateQRCodePropagatesErrors(t *testing.T) {
+	w := new(ErrorWriter)
+	err := GenerateQRCode(w, "555-2368")
+
+	if err == nil || err.Error() != "Expected error" {
+		t.Errorf("Error not propagated correctly, got %v", err)
 	}
 }
