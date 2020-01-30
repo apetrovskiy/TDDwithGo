a4bc9de8f25d56240a7c148edd5b5440e91f40a1
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-01-30 06:30:00 +0300

    red-green

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -1,14 +1,17 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"image/png"
+	"testing"
+)
 
 func TestGenerateQRCodeReturnsValue(t *testing.T) {
 	result := GenerateQRCode("555-2368")
+	buffer := bytes.NewBuffer(result)
+	_, err := png.Decode(buffer)
 
-	if result == nil {
-		t.Errorf("Generated QRCode is nil")
-	}
-	if len(result) == 0 {
-		t.Errorf("Generated QRCode has no data")
+	if err != nil {
+		t.Errorf("Generated QRCode is not a PNG: %s", err)
 	}
 }
